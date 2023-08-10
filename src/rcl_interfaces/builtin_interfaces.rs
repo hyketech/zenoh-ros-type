053f4b9f1edcd5ab0825c344cd2c7171580b3522
ChenYing Kuo <evshary@gmail.com>
2023-08-10 16:47:45 +0800

    Rename nsec to nanosec.

--- src/rcl_interfaces/builtin_interfaces.rs
+++ src/rcl_interfaces/builtin_interfaces.rs
@@ -3,11 +3,11 @@ use serde_derive::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Time {
     pub sec: i32,
-    pub nsec: u32,
+    pub nanosec: u32,
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Duration {
     pub sec: i32,
-    pub nsec: u32,
+    pub nanosec: u32,
 }
