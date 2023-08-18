0ee1d0d6df5e394481e4b24703fecce0fc4b41b6
ChenYing Kuo <evshary@gmail.com>
2023-08-18 14:08:03 +0800

    Fix the GateMode Enum.

--- src/tier4_autoware_msgs/tier4_control_msgs.rs
+++ src/tier4_autoware_msgs/tier4_control_msgs.rs
@@ -1,8 +1,8 @@
 use serde_derive::{Deserialize, Serialize};
 
 pub mod gate_mode_data {
-    pub const AUTO: u8 = 1;
-    pub const EXTERNAL: u8 = 2;
+    pub const AUTO: u8 = 0;
+    pub const EXTERNAL: u8 = 1;
 }
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct GateMode {
