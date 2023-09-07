0d0be3372aaae81a16edc56efe54de333372ed7f
ChenYing Kuo <evshary@gmail.com>
2023-09-07 16:43:16 +0800

    Update ControlModeReport, GearCommand and GearReport.

--- src/autoware_auto_msgs/autoware_auto_vehicle_msgs.rs
+++ src/autoware_auto_msgs/autoware_auto_vehicle_msgs.rs
@@ -14,6 +14,15 @@ pub struct ControlModeCommand {
     pub mode: u8,
 }
 
+pub mod control_mode_report {
+    pub const NO_COMMAND: u8 = 0;
+    pub const AUTONOMOUS: u8 = 1;
+    pub const AUTONOMOUS_STEER_ONLY: u8 = 2;
+    pub const AUTONOMOUS_VELOCITY_ONLY: u8 = 3;
+    pub const MANUAL: u8 = 4;
+    pub const DISENGAGED: u8 = 5;
+    pub const NOT_READY: u8 = 6;
+}
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct ControlModeReport {
     pub stamp: Time,
@@ -27,10 +36,31 @@ pub struct Engage {
 }
 
 pub mod gear_command {
+    pub const NONE: u8 = 0;
+    pub const NEUTRAL: u8 = 1;
     pub const DRIVE: u8 = 2;
+    pub const DRIVE_2: u8 = 3;
+    pub const DRIVE_3: u8 = 4;
+    pub const DRIVE_4: u8 = 5;
+    pub const DRIVE_5: u8 = 6;
+    pub const DRIVE_6: u8 = 7;
+    pub const DRIVE_7: u8 = 8;
+    pub const DRIVE_8: u8 = 9;
+    pub const DRIVE_9: u8 = 10;
+    pub const DRIVE_10: u8 = 11;
+    pub const DRIVE_11: u8 = 12;
+    pub const DRIVE_12: u8 = 13;
+    pub const DRIVE_13: u8 = 14;
+    pub const DRIVE_14: u8 = 15;
+    pub const DRIVE_15: u8 = 16;
+    pub const DRIVE_16: u8 = 17;
+    pub const DRIVE_17: u8 = 18;
+    pub const DRIVE_18: u8 = 19;
     pub const REVERSE: u8 = 20;
+    pub const REVERSE_2: u8 = 21;
     pub const PARK: u8 = 22;
     pub const LOW: u8 = 23;
+    pub const LOW_2: u8 = 24;
 }
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct GearCommand {
@@ -38,6 +68,33 @@ pub struct GearCommand {
     pub command: u8,
 }
 
+pub mod gear_report {
+    pub const NONE: u8 = 0;
+    pub const NEUTRAL: u8 = 1;
+    pub const DRIVE: u8 = 2;
+    pub const DRIVE_2: u8 = 3;
+    pub const DRIVE_3: u8 = 4;
+    pub const DRIVE_4: u8 = 5;
+    pub const DRIVE_5: u8 = 6;
+    pub const DRIVE_6: u8 = 7;
+    pub const DRIVE_7: u8 = 8;
+    pub const DRIVE_8: u8 = 9;
+    pub const DRIVE_9: u8 = 10;
+    pub const DRIVE_10: u8 = 11;
+    pub const DRIVE_11: u8 = 12;
+    pub const DRIVE_12: u8 = 13;
+    pub const DRIVE_13: u8 = 14;
+    pub const DRIVE_14: u8 = 15;
+    pub const DRIVE_15: u8 = 16;
+    pub const DRIVE_16: u8 = 17;
+    pub const DRIVE_17: u8 = 18;
+    pub const DRIVE_18: u8 = 19;
+    pub const REVERSE: u8 = 20;
+    pub const REVERSE_2: u8 = 21;
+    pub const PARK: u8 = 22;
+    pub const LOW: u8 = 23;
+    pub const LOW_2: u8 = 24;
+}
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct GearReport {
     pub stamp: Time,
