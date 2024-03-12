d4f1f703b0ef84538a1f99eb67ffc67bc3e805f7
HykeLuka <luka@hyke.no>
2024-03-12 10:10:03 +0100

    created single thruster command

--- src/hyke_vessel_msgs/hyke_vessel_control_msgs.rs
+++ src/hyke_vessel_msgs/hyke_vessel_control_msgs.rs
@@ -2,8 +2,13 @@ use crate::builtin_interfaces::Time;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
-pub struct HykeVesselCommand {
-    pub ids: Vec<i32>,
-    pub powers: Vec<f64>,
-    pub angles: Vec<f64>,
+pub struct HykeThrusterCommand {
+    pub i: i32, // index
+    pub u: f32, // power / force
+    pub alpha: f32, // angle
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
+pub struct HykeThrusterCommands {
+    pub thruster_command: Vec<HykeThrusterCommand>,
 }
