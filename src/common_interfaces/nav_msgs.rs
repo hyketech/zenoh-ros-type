8d994154c319e23af868f8db7d62f995da7af42f
HykeLuka <luka@hyke.no>
2024-03-14 12:33:11 +0100

    fixed namespace

--- src/common_interfaces/nav_msgs.rs
+++ src/common_interfaces/nav_msgs.rs
@@ -4,8 +4,8 @@ use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct Odometry {
-    pub header: Header,
+    pub header: std_msgs::Header,
     pub child_frame_id: String,
-    pub pose: PoseWithCovariance,
-    pub twist: TwistWithCovariance,
+    pub pose: geometry_msgs::PoseWithCovariance,
+    pub twist: geometry_msgs::TwistWithCovariance,
 }
